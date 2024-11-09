24803b431945e75f2d25d685f2f0339585bb3d05
Cristina <cristina.oana237@gmail.com>
2024-11-09 17:20:29 +0200

    Added the methods for the UI
    -to display the menus for logging in,
    -signing up,
    -as well as methods for the managing that the Admins can do
    - also added objects to the inMemoryRepository of the Admins and the Users

--- starmatch/src/ConsoleApp.java
+++ starmatch/src/ConsoleApp.java
@@ -1,6 +1,17 @@
+import model.Admin;
+import model.User;
+import repository.InMemoryRepository;
+import repository.Repository;
+
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class ConsoleApp {
+    private final StarMatchController starMatchController;
+
+    public ConsoleApp(StarMatchController starMatchController) {
+        this.starMatchController = starMatchController;
+    }
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
@@ -30,6 +41,9 @@ public class ConsoleApp {
                 case "2" -> {
                     adminLoginMenu(scanner);
                 }
+                case "3" -> {
+                    userSignUpMenu(scanner);
+                }
                 default -> System.out.println("Invalid option. Please try again.");
             }
         }
@@ -37,29 +51,54 @@ public class ConsoleApp {
     }
 
     private void userLoginMenu(Scanner scanner) {
-        System.out.println("""
-                -- User Log In --
-                Please enter your email and password.
-                Email:
-                Password:
-                """);
+        System.out.println("-- User Log In --");
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
 
-        // Simulate login success and display user menu
-        System.out.println("Login successful! Welcome, User.");
-        userMenu(scanner);
+        if (starMatchController.handleUserLogin(email, password)) {
+            System.out.println("Login successful! Welcome, " + email);
+            userMenu(scanner);
+        } else {
+            System.out.println("Invalid credentials. Please try again.");
+        }
     }
 
     private void adminLoginMenu(Scanner scanner) {
-        System.out.println("""
-                -- Admin Log In --
-                Please enter your email and password.
-                Email:
-                Password:
-                """);
+        System.out.println("-- Admin Log In --");
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
 
-        // Simulate login success and display admin menu
-        System.out.println("Login successful! Welcome, Admin.");
-        adminMenu(scanner);
+        if (starMatchController.handleAdminLogin(email, password)) {
+            System.out.println("Login successful! Welcome, " + email);
+            adminMenu(scanner);
+        } else {
+            System.out.println("Invalid credentials. Please try again.");
+        }
+    }
+
+    private void userSignUpMenu(Scanner scanner) {
+        System.out.println("-- User Sign Up --");
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        System.out.print("Birthdate (yyyy-MM-dd): ");
+        String dateOfBirth = scanner.nextLine();
+        System.out.print("Time of birth: ");
+        String timeOfBirth = scanner.nextLine();
+        System.out.print("Place of birth: ");
+        String placeOfBirth = scanner.nextLine();
+
+        starMatchController.signUpNewUser(name, LocalDate.parse(dateOfBirth), Integer.valueOf(timeOfBirth), placeOfBirth, email, password);
+
+        System.out.println("Sign-up successful! Welcome, " + name);
+        userMenu(scanner);
     }
 
     private void userMenu(Scanner scanner) {
@@ -103,6 +142,7 @@ public class ConsoleApp {
                     1. Manage Users
                     2. Manage Traits
                     3. Manage Quotes
+                    4. Manage Admins
                     
                     0. Log Out
                     """);
@@ -117,13 +157,76 @@ public class ConsoleApp {
                 case "1" -> System.out.println("Managing users...");
                 case "2" -> System.out.println("Managing traits...");
                 case "3" -> System.out.println("Managing quotes...");
+                case "4" -> adminManageAdminMenu(scanner);
                 default -> System.out.println("Invalid option. Please try again.");
             }
         }
     }
 
+    private void adminManageAdminMenu(Scanner scanner) {
+        boolean adminLoop = true;
+        while (adminLoop) {
+            System.out.print("""
+                    -- Manage other Admins --
+                    1. View all admins
+                    2. Add new admin
+                    3. Delete admin
+                    4. Update existing admin
+                    
+                    0. Go back to main menu
+                    """);
+
+            String adminOption = scanner.nextLine();
+
+            switch (adminOption) {
+                case "0" -> adminLoop = false;
+                case "1" -> starMatchController.viewAdmins();
+                case "2" -> addNewAdmin(scanner);
+                case "3" -> System.out.println("deleting admin...");
+                case "4" -> System.out.println("update existing admin...");
+                default -> System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    private void addNewAdmin(Scanner scanner) {
+        System.out.println("-- Add New Admin --");
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        starMatchController.addNewAdmin(name, email, password);
+        System.out.println("Admin added successfully!");
+    }
+
     public static void main(String[] args) {
-        ConsoleApp consoleApp = new ConsoleApp();
+        Repository<User> userRepository = createInMemoryUserRepository();
+        Repository<Admin> adminRepository = createInMemoryAdminRepository();
+
+        StarMatchService starMatchService = new StarMatchService(userRepository, adminRepository);
+        StarMatchController starMatchController = new StarMatchController(starMatchService);
+
+        ConsoleApp consoleApp = new ConsoleApp(starMatchController);
         consoleApp.start();
+    }
+
+
+    private static Repository<User> createInMemoryUserRepository() {
+        Repository<User> userRepository = new InMemoryRepository<>();
+        userRepository.create(new User(1, "Amna", LocalDate.of(2000, 3, 12), 9, "Romania", "amna@gmail.com", "parola"));
+        userRepository.create(new User(2, "Florian", LocalDate.of(2007, 7, 24), 10, "Romania", "florinel@gmail.com", "0987"));
+        userRepository.create(new User(3, "Briana Gheorghe", LocalDate.of(2004, 1, 3), 22, "USA", "brianaagheorghe@yahoo.com", "bribri"));
+        userRepository.create(new User(4, "sore marian", LocalDate.of(1990, 9, 10), 6, "Germania", "soremarian@gmail.com", "sore1"));
+        return userRepository;
+    }
+
+    private static Repository<Admin> createInMemoryAdminRepository() {
+        Repository<Admin> adminRepository = new InMemoryRepository<>();
+        adminRepository.create(new Admin(1, "Bogdan Popa", "bogdan.popa@yahoo.com", "1234"));
+        adminRepository.create(new Admin(2, "Ioana Popa", "ioana.popa@yahoo.com" , "1234"));
+        return adminRepository;
     }
 }
