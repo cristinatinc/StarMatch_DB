d9153e09b4ba03e0b049314f70c472edc3645d26
Cristina <cristina.oana237@gmail.com>
2024-11-09 17:18:11 +0200

    Added methods to Service and Controller to implement the logging in as Admin/ User, the signing up as User, as well as the viewing of Admins and Users

--- starmatch/src/StarMatchController.java
+++ starmatch/src/StarMatchController.java
@@ -1,4 +1,30 @@
+import java.time.LocalDate;
+
 public class StarMatchController {
-    public StarMatchController() {
+
+    private final StarMatchService starMatchService;
+
+    public StarMatchController(StarMatchService starMatchService) {this.starMatchService=starMatchService;}
+
+    public boolean handleUserLogin(String email, String password) {
+        return starMatchService.validateUserLogin(email, password);
+    }
+
+    public boolean handleAdminLogin(String email, String password) {
+        return starMatchService.validateAdminLogin(email, password);
+    }
+
+    public void signUpNewUser(String name, LocalDate birthDate, Integer birthTime, String birthPlace, String email, String password) {
+        starMatchService.createUser(name, birthDate, birthTime, birthPlace, email, password);
+    }
+
+    public void addNewAdmin(String name, String email, String password){
+        starMatchService.createAdmin(name, email, password);
+    }
+
+    public void viewAdmins(){
+        StringBuilder output = new StringBuilder("Admins of the app:\n");
+        starMatchService.getAdmins().forEach(course -> output.append(course.toString()).append("\n"));
+        System.out.println(output);
     }
 }
--- starmatch/src/StarMatchService.java
+++ starmatch/src/StarMatchService.java
@@ -2,15 +2,47 @@ import model.Admin;
 import model.User;
 import repository.Repository;
 
-public class StarMatchService {
-    private Repository<User> userRepository;
+import java.time.LocalDate;
+import java.util.List;
 
-    private Repository<Admin> adminRepository;
+public class StarMatchService {
+    private final Repository<User> userRepository;
+    private final Repository<Admin> adminRepository;
 
     public StarMatchService(Repository<User> userRepository, Repository<Admin> adminRepository) {
         this.userRepository = userRepository;
         this.adminRepository = adminRepository;
     }
 
+    public boolean validateUserLogin(String email, String password) {
+        return userRepository.getAll().stream()
+                .anyMatch(user -> user.getEmail().equals(email) && user.getPassword().equals(password));
+    }
+
+    public boolean validateAdminLogin(String email, String password) {
+        return adminRepository.getAll().stream()
+                .anyMatch(admin -> admin.getEmail().equals(email) && admin.getPassword().equals(password));
+    }
+
+    public void createUser(String name, LocalDate birthDate, Integer birthTime, String birthPlace, String email, String password) {
+        User newUser = new User(getMaxUserId() + 1, name, birthDate, birthTime, birthPlace, email, password);
+        userRepository.create(newUser);
+    }
+
+    public void createAdmin(String name, String email, String password) {
+        Admin newAdmin = new Admin(getMaxUserId() + 1, name, email, password);
+        adminRepository.create(newAdmin);
+    }
+
+    public int getMaxUserId() {
+        return userRepository.getAll().stream()
+                .mapToInt(User::getId)
+                .max()
+                .orElse(0);
+    }
+
+    public List<Admin> getAdmins() { return adminRepository.getAll();}
+
+    public List<User> getUsers() { return userRepository.getAll();}
 
 }
