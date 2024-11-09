bd7813842e2ecb3c7805944347aa2ac3dcf688d4
Cristina <cristina.oana237@gmail.com>
2024-11-09 17:14:01 +0200

    Modified Admin class to not extend User anymore, but to implement HasId
    - added setters, getters, constructor and toString methods

--- starmatch/src/model/Admin.java
+++ starmatch/src/model/Admin.java
@@ -1,10 +1,58 @@
 package model;
 
-import java.util.Date;
-import java.util.List;
+public class Admin implements HasId{
+    private int id;
+    private String name;
+    private String password;
+    private String email;
 
-public class Admin extends User{
-    public Admin(int id, String name, Date birthDate, Integer birthTime, String birthPlace, String email, String password, List<Friend> friends) {
-        super(id, name, birthDate, birthTime, birthPlace, email, password, friends);
+    public Admin(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
